c930dfb53d3d135a5933105379d34edf4faf1c42
abitogonnet <abito.gonnet@gmail.com>
2026-01-30 13:45:56 -0300

    Update urls.py

--- gastos/urls.py
+++ gastos/urls.py
@@ -6,4 +6,5 @@ app_name = "gastos"
 urlpatterns = [
     path("", views.home, name="home"),
     path("crear/", views.crear, name="crear"),
+    path("division-bienes/", views.division_bienes, name="division_bienes"),
 ]
