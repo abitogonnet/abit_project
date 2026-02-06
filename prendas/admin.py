3b641928d4f2832be495a02348a107de105374d7
abitogonnet <abito.gonnet@gmail.com>
2026-02-06 17:53:08 -0300

    Delete prendas directory

--- prendas/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Prenda
-
-@admin.register(Prenda)
-class PrendaAdmin(admin.ModelAdmin):
-    list_display = ("codigo", "categoria", "marca", "color", "talle", "estado")
-    list_filter = ("categoria", "estado", "marca", "color")
-    search_fields = ("codigo", "marca", "color", "talle")
