7fed769962270dbab0c0b8745b707d6365c2547c
abitogonnet <abito.gonnet@gmail.com>
2026-02-06 17:58:11 -0300

    Delete gastos directory

--- gastos/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Gasto
-
-@admin.register(Gasto)
-class GastoAdmin(admin.ModelAdmin):
-    list_display = ("fecha", "categoria", "metodo", "descripcion", "monto")
-    list_filter = ("categoria", "metodo", "fecha")
-    search_fields = ("descripcion",)
