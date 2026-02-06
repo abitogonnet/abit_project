4fd35169cc6d60894b12e9571eabd00d5bb6cd89
abitogonnet <abito.gonnet@gmail.com>
2026-02-06 17:54:46 -0300

    Add files via upload

---
+++ alquileres/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Alquiler, AlquilerItem
+
+class AlquilerItemInline(admin.TabularInline):
+    model = AlquilerItem
+    extra = 0
+
+@admin.register(Alquiler)
+class AlquilerAdmin(admin.ModelAdmin):
+    list_display = ("id", "cliente_nombre", "fecha_entrega", "fecha_devolucion", "estado_alquiler", "estado_saldo", "total_final")
+    list_filter = ("estado_alquiler", "estado_saldo")
+    search_fields = ("cliente_nombre", "cliente_telefono")
+    inlines = [AlquilerItemInline]
