6728ed4e694243bd96827c6a074d566bcb697c70
abitogonnet <abito.gonnet@gmail.com>
2026-03-06 18:22:06 -0300

    Delete reportes directory

--- reportes/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "reportes"
-
-urlpatterns = [
-    path("", views.home, name="home"),
-    path("exportar/", views.exportar_excel, name="exportar_excel"),
-]
