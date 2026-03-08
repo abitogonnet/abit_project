29c2677a43af3fa6edcfd97ebf985226898e8271
abitogonnet <abito.gonnet@gmail.com>
2026-03-08 19:25:09 -0300

    urls.py

--- prendas/urls.py
+++ prendas/urls.py
@@ -6,5 +6,6 @@ app_name = "prendas"
 urlpatterns = [
     path("crear/", views.crear_prenda, name="crear"),
     path("stock/", views.stock, name="stock"),
+    path("eliminar/<int:prenda_id>/", views.eliminar_prenda, name="eliminar"),
     path("buscar/", views.buscar_codigo, name="buscar_codigo"),
 ]
